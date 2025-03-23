d3789ad5094010d1469b64fd2a531c2e990d5100
Diana Plesa <diana.plesa04@gmail.com>
2025-03-23 14:20:52 +0200

    added FIND NEW comment

--- code/game-dev/Space Invaders/scripts/Bullet.cs
+++ code/game-dev/Space Invaders/scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
+    //FIND NEW
 
     public float moveSpeed;
     public GameObject ExplosionPrefab;
